8018779d389ef92c04bab9ce3c636884ab277201
johnLee1501 <jhonjhad@gmail.com>
2021-09-19 11:12:58 -0500

    mejoras

--- microservicio/infraestructura/src/test/java/com/ceiba/producto/controlador/ComandoControladorProductoTest.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/producto/controlador/ComandoControladorProductoTest.java
@@ -2,6 +2,8 @@ package com.ceiba.producto.controlador;
 
 import com.ceiba.ApplicationMock;
 import com.ceiba.producto.comando.ComandoProducto;
+import com.ceiba.producto.modelo.dto.DtoProducto;
+import com.ceiba.producto.puerto.dao.DaoProducto;
 import com.ceiba.producto.servicio.testdatabuilder.ComandoProductoTestDataBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
@@ -13,7 +15,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static com.ceiba.producto.BasePrueba.assertThrows;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,17 +33,29 @@ public class ComandoControladorProductoTest {
     @Autowired
     private MockMvc mocMvc;
 
+    @Autowired
+    private DaoProducto daoProducto;
+
     @Test
     public void crear() throws Exception {
         // arrange
         ComandoProducto producto = new ComandoProductoTestDataBuilder().build();
-
         // act - assert
         mocMvc.perform(post("/productos")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(producto)))
                 .andExpect(status().isOk())
-                .andExpect(content().json("{'valor': 4}"));
+                .andExpect(content().json("{'valor': 4}"))
+                .andDo(
+                        resultValorar -> {
+                            DtoProducto producto1 = daoProducto.obtenerPorId(4L);
+                            assertEquals("HSJDH9", producto1.getCodigoProducto());
+                            assertEquals("Xiaomi Note 9", producto1.getNombre());
+                            assertEquals(140000, producto1.getValorProducto());
+                        }
+                );
+
+        ;
     }
 
 
@@ -51,6 +68,11 @@ public class ComandoControladorProductoTest {
         mocMvc.perform(delete("/productos/{id}", id)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(
+                        resultValorar -> {
+                            assertThrows(() -> daoProducto.obtenerPorId(4L), IndexOutOfBoundsException.class, "Index: 0, Size: 0");
+                        }
+                );
     }
 }
